518e24bf7a5a157e9ee84605db7365c61ab6260f
M2D_ISE03 <gd_ap_tr@tp_git.com>
2016-11-07 14:48:13 +0100

    BluetoothAdapter
    Implémentation de la vérification du bluetooth sur l'appareil au
    lancement de l'application

--- app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
+++ app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.gaut.appbletooth;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
         mScanButton = (Button) findViewById(R.id.scan_button);
         mBluetoothButton = (Button) findViewById(R.id.bluetooth_button);
+        
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null)
+            Toast.makeText(MainActivity.this, "Pas de Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Avec Bluetooth",
+                    Toast.LENGTH_SHORT).show();
 
         mScanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
