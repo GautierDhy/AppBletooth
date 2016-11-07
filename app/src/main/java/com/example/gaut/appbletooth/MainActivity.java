e7a9bdd4132ba34e733dcd72ed25bba1fd52dd6a
M2D_ISE03 <gd_ap_tr@tp_git.com>
2016-11-07 12:05:31 +0100

    Button
    Interface graphique du bouton (vide)

--- app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
+++ app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.gaut.appbletooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBluetoothButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
