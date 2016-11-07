163fe6fdcb60f2ba2e0e38cc2ede8eb88c946f05
M2D_ISE03 <gd_ap_tr@tp_git.com>
2016-11-07 15:30:22 +0100

    Vérification et activation Bluetooth
    Ajout permission bluetooth admin
    Inversion place des deux boutons
    Vérification de l'option BT sur le device
    Demande d'activation si bluetooth desactivé

--- app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
+++ app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gaut.appbletooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     Button mBluetoothButton;
     Button mScanButton;
 
+    BluetoothAdapter mbluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +24,15 @@ public class MainActivity extends AppCompatActivity {
 
         mScanButton = (Button) findViewById(R.id.scan_button);
         mBluetoothButton = (Button) findViewById(R.id.bluetooth_button);
-        
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null)
+
+        if (mbluetoothAdapter == null)
             Toast.makeText(MainActivity.this, "Pas de Bluetooth",
                     Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(MainActivity.this, "Avec Bluetooth",
                     Toast.LENGTH_SHORT).show();
+
+
 
         mScanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +44,23 @@ public class MainActivity extends AppCompatActivity {
         mBluetoothButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                if (!mbluetoothAdapter.isEnabled()) {
+                    Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+                }
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
+            return;
+        if (resultCode == RESULT_OK) {
+            // L'utilisation a activé le bluetooth
+        } else {
+            // L'utilisation n'a pas activé le bluetooth
+        }
+    }
+
 }
