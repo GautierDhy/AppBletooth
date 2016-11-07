9cd6c684f8137337089273e38956b88c13628aab
M2D_ISE03 <gd_ap_tr@tp_git.com>
2016-11-07 14:25:00 +0100

    Button 2
    Interface graphique du deuxième button

--- app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
+++ app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.gaut.appbletooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mBluetoothButton;
-    
+    Button mScanButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScanButton = (Button) findViewById(R.id.scan_button);
+        mBluetoothButton = (Button) findViewById(R.id.bluetooth_button);
+
+        mScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        mBluetoothButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
