cb4b70dd8753d3862ba3f4d6a092c06e097f215a
M2D_ISE03 <gd_ap_tr@tp_git.com>
2016-11-14 17:38:55 +0100

    GIF
    Gif lent mais fonctionnel

---
+++ app/src/main/java/com/example/gaut/appbletooth/GifFrames.java
@@ -0,0 +1,101 @@
+package com.example.gaut.appbletooth;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Arthur Parmelan on 14/11/2016.
+ */
+
+public class GifFrames {
+
+    public static Bitmap getGifs(Context ctx, int value) {
+
+        List<Bitmap> gifs = new ArrayList<>();
+
+        int realValue = value%48;
+        int arrayNumber = realValue/10;
+        int indice = realValue%10;
+
+        if (value == 48){
+            if (indice > 8) {
+                indice = 8;
+            }
+        }
+
+        switch (arrayNumber){
+            case 0:
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_0_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_1_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_2_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_3_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_4_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_5_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_6_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_7_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_8_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_9_delay08s));
+
+                break;
+            case 1:
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_10_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_11_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_12_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_13_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_14_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_15_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_16_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_17_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_18_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_19_delay08s));
+                break;
+            case 2:
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_20_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_21_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_22_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_23_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_24_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_25_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_26_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_27_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_28_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_29_delay08s));
+                break;
+            case 3:
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_30_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_31_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_32_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_33_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_34_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_35_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_36_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_37_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_38_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_39_delay08s));
+                break;
+            case 4:
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_40_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_41_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_42_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_43_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_44_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_45_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_46_delay08s));
+                gifs.add(BitmapFactory.decodeResource(ctx.getResources(),R.drawable.frame_47_delay08s));
+                break;
+            default:
+                break;
+        }
+
+
+
+        return gifs.get(indice);
+    }
+
+
+}
--- app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
+++ app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
@@ -6,15 +6,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +33,11 @@ public class MainActivity extends AppCompatActivity {
     Button mScanButton;
     ArrayList<String> mArrayAdapter = new ArrayList<>();
     ListView mDeviceListView;
-
+    ImageView mAnimImageView;
     BluetoothAdapter mbluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
-
+    int nextBitmap = 0;
+    int stopBitmap = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mAnimImageView = (ImageView) findViewById(R.id.iv_animation);
+
     }
 
     @Override
@@ -81,10 +96,28 @@ public class MainActivity extends AppCompatActivity {
     private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
+
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (stopBitmap == 1){
+                        mAnimImageView.setImageBitmap(null);
+                    }
+                    else {
+                        mAnimImageView.setImageBitmap(GifFrames.getGifs(getApplicationContext(),nextBitmap));
+                        nextBitmap++;
+                        handler.postDelayed(this,200);
+                    }
+                }
+            },0);
+
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 Toast.makeText(MainActivity.this, "New Device = " + device.getName(), Toast.LENGTH_SHORT).show();
                 mArrayAdapter.add(device.getName() + " " + device.getAddress());
+                stopBitmap = 0;
             }
             if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(
@@ -92,15 +125,53 @@ public class MainActivity extends AppCompatActivity {
                         R.layout.simple_list1,
                         mArrayAdapter
                 );
-                    mDeviceListView.setAdapter(stringArrayAdapter);
+                mDeviceListView.setAdapter(stringArrayAdapter);
+                stopBitmap = 1;
             }
         }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void checkIfAnimationDone(final AnimationDrawable anim){
+        final AnimationDrawable a = anim;
+        int timeBetweenChecks = 100;
+        Handler h = new Handler();
+        h.postDelayed(new Runnable(){
+            public void run(){
+                if (a.getCurrent() != a.getFrame(a.getNumberOfFrames() - 1)){
+                    checkIfAnimationDone(a);
+                } else{
+
+                    mAnimImageView.setBackgroundDrawable(null);
+                    mAnimImageView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            AnimationDrawable frameAnimation = new AnimationDrawable();
+                            Random g = new Random();
+
+
+                            mAnimImageView.setBackgroundDrawable(frameAnimation);
+
+                            frameAnimation.start();
+                            checkIfAnimationDone(frameAnimation);
+                        }
+                    });
+                }
+            }
+        }, timeBetweenChecks);
     };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mbluetoothAdapter.cancelDiscovery();
-        unregisterReceiver(bluetoothReceiver);
+        if(bluetoothReceiver != null) {
+           // unregisterReceiver(bluetoothReceiver);
+        }
     }
 }
