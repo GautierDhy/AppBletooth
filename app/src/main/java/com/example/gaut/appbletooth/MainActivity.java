9a6fd9bbeb0fb81445bf18caa81b462596cebea0
M2D_ISE03 <gd_ap_tr@tp_git.com>
2016-12-02 17:20:46 +0100

    Projet final
    Projet avec maps fonctionnant

--- app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
+++ app/src/main/java/com/example/gaut/appbletooth/MainActivity.java
@@ -13,7 +13,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,7 +25,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.example.gaut.appbletooth.activities.MapActivity;
+import com.example.gaut.appbletooth.data.GPSData;
+import com.example.gaut.appbletooth.interfaces.IGPSData;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -44,6 +52,30 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
     int nextBitmap = 0;
     int stopBitmap = 0;
+
+
+    //---------------------------------------------------------------
+    Handler mHandler;
+    Button mMapActivityButton;
+    TextView mPositionGPSTextView;
+
+    IGPSData myPositionGPSData;
+
+    Runnable mUIRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            Location location = myPositionGPSData.getPosition();
+
+            mPositionGPSTextView.setText(" ");
+
+            mHandler.postDelayed(this,1000);
+        }
+    };
+
+//---------------------------------------------------------------
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
                 registerReceiver(bluetoothReceiver, filter);
                 mbluetoothAdapter.startDiscovery();
+                Toast.makeText(MainActivity.this, "Scan en cours", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -79,6 +112,9 @@ public class MainActivity extends AppCompatActivity {
                 if (!mbluetoothAdapter.isEnabled()) {
                     Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Bluetooth déjà activé", Toast.LENGTH_SHORT).show();;
                 }
             }
         });
@@ -94,6 +130,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+//---------------------------------------------------------------
+        mPositionGPSTextView = (TextView) findViewById(R.id.gps_position_textview);
+        mMapActivityButton = (Button) findViewById(R.id.map_activity_button);
+
+        mMapActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mapActivityIntent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(mapActivityIntent);
+            }
+        });
+
+
+        myPositionGPSData = new GPSData();
+
+        mHandler = new Handler(Looper.getMainLooper());
+
+//---------------------------------------------------------------
     }
 
     @Override
@@ -112,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
-            final GifFrames gifFrames = new GifFrames();
+          /*  final GifFrames gifFrames = new GifFrames();
             final Handler handler = new Handler();
             stopBitmap = 0;
             handler.postDelayed(new Runnable() {
@@ -131,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 }
-            },100);
+            },100);*/
 
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
@@ -147,12 +203,19 @@ public class MainActivity extends AppCompatActivity {
                         mArrayAdapter
                 );
                 stopBitmap = 1;
-                    mDeviceListView.setAdapter(new ArrayAdapter<String>(context,android.R.layout.simple_expandable_list_item_1,mArrayAdapter));
+                    mDeviceListView.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_expandable_list_item_1, mArrayAdapter));
+                Toast.makeText(MainActivity.this, "Scan fini", Toast.LENGTH_LONG).show();
             }
         }
 
     };
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mHandler.post(mUIRunnable);
+    }
 
     @Override
     protected void onStart() {
@@ -187,7 +250,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }, timeBetweenChecks);
-    };
+    }
 
     @Override
     protected void onDestroy() {
@@ -197,4 +260,15 @@ public class MainActivity extends AppCompatActivity {
            // unregisterReceiver(bluetoothReceiver);
         }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 }
---
+++ app/src/main/java/com/example/gaut/appbletooth/interfaces/IGPSData.java
@@ -0,0 +1,12 @@
+package com.example.gaut.appbletooth.interfaces;
+
+import android.location.Location;
+
+/**
+ * Created by Arthur Parmelan on 22/11/2016.
+ */
+
+public interface IGPSData {
+
+    Location getPosition();
+}
